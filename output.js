49bc8b57b52ce23f362c02c168302e3a6b57150a
Dk1446 <kayal.debopam.14@gmail.com>
2019-01-15 10:20:55 +0530

    swap numbers

--- output.js
+++ output.js
@@ -19,3 +19,16 @@ function multiply(){
 function hello(){
     alert("Hello World");
 }
+
+function swap(){
+    var a,b,c;
+    a=Number(document.getElementById("first").value);
+    b=Number(document.getElementById("second").value);
+
+    c=a;
+    a=b;
+    b=c;
+    document.getElementById("answer1").value=a;
+    document.getElementById("answer2").value=b;
+
+}
