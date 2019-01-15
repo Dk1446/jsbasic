87accf02f524543bfa65eb8e617cd5529f21c464
Dk1446 <kayal.debopam.14@gmail.com>
2019-01-15 09:49:59 +0530

    output of multiplication

--- output.js
+++ output.js
@@ -4,5 +4,7 @@ function add(){
     a=Number(document.getElementById("first").value);
     b=Number(document.getElementById("second").value);
     c= a + b;
+    c=a*b;
     document.getElementById("answer").value= c;
 }
+
